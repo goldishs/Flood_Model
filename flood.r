ec5bce6b41bd63956badbf32129cc40bb3e03d9e
ely <elycbiggs@yahoo.com>
2016-07-20 13:03:00 -0400

    added a second layer
    this sadly means we need to assume some stupid crazy things.

--- flood.r
+++ flood.r
@@ -21,7 +21,7 @@
 require(raster)
 require(fields)
 source("Helperfunctions.R")
-set.seed(100) # makes the random not random 
+set.seed(100) # makes the random not random
 
 
 
@@ -31,12 +31,12 @@ set.seed(100) # makes the random not random
 ####################################################
 
 #dimensions of our grid
-n=100#rows
-m=100#columns
-iter = 75# number of iterations 
-# iter = 75 works well for p_S of 0.05 When elevation is constant. 
-# when n =100 m=100 and iter =75 the code takes about 1:33 seconds to compile. 
-# our code is extreamly slow. 
+n = 40#rows
+m = 40#columns
+iter = 30# number of iterations
+# iter = 75 works well for p_S of 0.05 When elevation is constant.
+# when n =100 m=100 and iter =75 the code takes about 1:33 seconds to compile.
+# our code is extreamly slow.
 
 
 #elevation and surface water matrix initialization
@@ -46,28 +46,29 @@ iter = 75# number of iterations
 # Create 100x100 random raster with a Z range of 500-1500
 
 #InitialElevation <- raster(ncols=m, nrows=n, xmn=0)
-#InitialElevation[] <- runif(ncell(InitialElevation), min=0, max=500)  
+#InitialElevation[] <- runif(ncell(InitialElevation), min=0, max=500)
 
 
 ###############################################################################################
 
-# trying to creat a new elivaiton matrix that looks more natural. 
-# i add 8 to the dimensions because the focal function surrounds the 
+# trying to creat a new elivaiton matrix that looks more natural.
+# i add 8 to the dimensions because the focal function surrounds the
 # r.sim matrix with NA values. so i add 8 to each so that asdg becomes an n X m matrix
 
 # declaring raster
-r <- raster(ncols=m+8, nrows=n+8, xmn=0)
-r[] <- runif(ncell(r), min=0, max=1) 
+r <- raster(ncols = m + 8, nrows = n + 8, xmn = 0)
+r[] <- runif(ncell(r), min = 0, max = 1)
 #this part makes the elevations look more natural
-r.sim <- focal(r, w=GaussianKernel(sigma=1, n=9), na.rm = TRUE)
-# Convert to a matrix 
+r.sim <- focal(r, w = GaussianKernel(sigma = 1, n = 9), na.rm = TRUE)
+# Convert to a matrix
 asdf = as.matrix(r.sim)
 # Extracting the non-NA values of the matrix
 asdg = extraction(asdf)
 ###############################################################################################
 
-InitialSurfaceWater <- raster(ncols=m, nrows=n, xmn=0)
-InitialSurfaceWater[] <- runif(ncell(InitialSurfaceWater), min=0, max=50)  
+InitialSurfaceWater <- raster(ncols = m, nrows = n, xmn = 0)
+InitialSurfaceWater[] <-
+  runif(ncell(InitialSurfaceWater), min = 0, max = 0.2)
 
 # E_data = c(5,10,10,8,25,30,20,40,25)
 # S_initial = c(5,2,1,1.5,2,1.1,4,2.1,0)
@@ -75,81 +76,100 @@ InitialSurfaceWater[] <- runif(ncell(InitialSurfaceWater), min=0, max=50)
 
 # E_data = InitialElevation[]
 E_data = asdg
-S_initial = InitialSurfaceWater[]
+#S_initial = InitialSurfaceWater[]
 
-E = matrix(E_data,nrow=n,ncol=m,byrow=TRUE)
-E = borderize(E,1700)
+# E = matrix(E_data,nrow=n,ncol=m,byrow=TRUE)
+# E = borderize(E,1700)
 
+E = E_data
+E = borderize(E,-100000)
 #surface water
-S = matrix(S_initial,nrow=n,ncol=m,byrow=TRUE)
+#S = matrix(S_initial,nrow=n,ncol=m,byrow=TRUE)
+S = matrix(runif(n * m), n, m)
 S = borderize(S,0)
 
 n = dim(E)[1]
 m = dim(E)[2]
 #change in surface water
-delta_S = matrix(rep(0,(n)*(m)),nrow=n,ncol=m)
+delta_S = matrix(rep(0,(n) * (m)),nrow = n,ncol = m)
 
-# change in time 
+#initializing the first second layer uniformly
+P_W = matrix(runif(n * m), n, m)
+
+
+# change in time
 dt = 1.00
 
 
 
 #loop through the matrices and update surface water
-image.plot(S, zlim = c(0.0, 50))
+image.plot(S, zlim = c(0.0, 0.7))
 
 for (k in 1:iter) {
-  
   #change in surface water
-  delta_S = matrix(rep(0,(n)*(m)),nrow=n,ncol=m)
+  delta_S = matrix(rep(0,(n) * (m)),nrow = n,ncol = m)
   
   # This is the percentage of S that leaves S_(i,j)
   p_S = 0.05
   
   
-  for (i in 2:(n-1)) {
-    for (j in 2:(m-1)) {
-      d=delta_ES(E,S,i,j)
-      r=ratio(d)
+  for (i in 2:(n - 1)) {
+    for (j in 2:(m - 1)) {
+      d = delta_ES(E,S,i,j)
+      r = ratio(d)
       Center = S[i,j]
-      rt = sum(r)
-
-      delta_S[i-1,j-1] = delta_S[i-1,j-1] + r[1,1]*Center *p_S  
-      delta_S[i-1,j] = delta_S[i-1,j] + r[1,2]*Center *p_S  
-      delta_S[i-1,j+1] = delta_S[i-1,j+1] + r[1,3]*Center *p_S  
-      delta_S[i,j-1] = delta_S[i,j-1] + r[2,1]*Center *p_S  
-      delta_S[i,j] = delta_S[i,j] + r[2,2]*Center *p_S  
-      delta_S[i,j+1] = delta_S[i,j+1] + r[2,3]*Center *p_S  
-      delta_S[i+1,j-1] = delta_S[i+1,j-1] + r[3,1]*Center *p_S  
-      delta_S[i+1,j] = delta_S[i+1,j] + r[3,2]*Center *p_S  
-      delta_S[i+1,j+1] = delta_S[i+1,j+1] + r[3,3]*Center *p_S   
+      #rt = sum(r)
       
-      t = (r[1,1]*Center*p_S + r[1,2]*Center*p_S+ r[1,3]*Center*p_S+ r[2,1]*Center*p_S+ 
-             r[2,3]*Center*p_S+ r[3,1]*Center*p_S+ r[3,2]*Center*p_S+ r[3,3]*Center*p_S)
+      delta_S[i - 1,j - 1] = delta_S[i - 1,j - 1] + r[1,1] * Center * p_S
+      delta_S[i - 1,j] = delta_S[i - 1,j] + r[1,2] * Center * p_S
+      delta_S[i - 1,j + 1] = delta_S[i - 1,j + 1] + r[1,3] * Center * p_S
+      delta_S[i,j - 1] = delta_S[i,j - 1] + r[2,1] * Center * p_S
+      delta_S[i,j] = delta_S[i,j] + r[2,2] * Center * p_S
+      delta_S[i,j + 1] = delta_S[i,j + 1] + r[2,3] * Center * p_S
+      delta_S[i + 1,j - 1] = delta_S[i + 1,j - 1] + r[3,1] * Center * p_S
+      delta_S[i + 1,j] = delta_S[i + 1,j] + r[3,2] * Center * p_S
+      delta_S[i + 1,j + 1] = delta_S[i + 1,j + 1] + r[3,3] * Center * p_S
       
-      delta_S[i,j] = delta_S[i,j] - t  
-
+      t = (
+        r[1,1] * Center * p_S + r[1,2] * Center * p_S + r[1,3] * Center * p_S + r[2,1] *
+          Center * p_S +
+          r[2,3] * Center * p_S + r[3,1] * Center * p_S + r[3,2] * Center *
+          p_S + r[3,3] * Center * p_S
+      )
+      
+      delta_S[i,j] = delta_S[i,j] - t
+      
     }
   }
   
   S = S + delta_S
-
-
-  # this function plots the new S maxtrix for every iteration. 
-  # note: when dt = 1 and when inital E and S are simple (constant/flat) 
-  # then delta_S will be 0 for all iterations. which is expected. 
+  
+  
+  # calling the delta water moving to layer 2.
+  dP_W = deltaP_W(S, P_W, C = 0.01)
+  
+  # subtracting surface water from layer 1
+  S = S - dP_W
+  #updating water in layer 2
+  P_W = P_W + dP_W
+  
+  # this function plots the new S maxtrix for every iteration.
+  # note: when dt = 1 and when inital E and S are simple (constant/flat)
+  # then delta_S will be 0 for all iterations. which is expected.
   
   # Also note:
-  # when dt =1 and when intial E  is simple (constant/flat)
-  # then S will Flip back and forth between two states. meaning 
-  # that i dont think it will ever converge (i.e. even out) 
+  # when p_S =1 and when intial E  is simple (constant/flat)
+  # then S will Flip back and forth between two states. meaning
+  # that i dont think it will ever converge (i.e. even out)
   
-  # zlim = c(min, max) manually adjust the color scale for each image. 
+  # zlim = c(min, max) manually adjust the color scale for each image.
   # thus the max needs to be the true max to the scale all of the images correctly
   
   # i want to move this out side of the loop to see if this speeds up the code.
-  image.plot(S, zlim = c(0.0, 210))
+  image.plot(S, zlim = c(0.0, 1))
 }
 
-# moving this outside the loop does provide some speed up. 
+# moving this outside the loop does provide some speed up.
 #image.plot(S, zlim = c(0.0, 200))
 print(S)
+image.plot(E, zlim = c(0.0, 1))
