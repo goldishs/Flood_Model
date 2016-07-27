cb44f0492f93b6f5c75305ced001bc92a0dbebf6
goldishs <simchagoldish@gmail.com>
2016-07-27 11:33:10 -0400

    add boiler plate comments

--- flood.m
+++ flood.m
@@ -1,1 +1,22 @@
-%not empty
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%Math 3800
+%Seth Goldish, Ely Biggs, Taylor Saintable
+%
+%Flood Model     Main program    Version 0.02
+%
+%
+%
+%
+%
+%
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%   START OF MAIN PROGRAM
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%dimensions of our grid
+n = 100;%rows
+m = 100;%columns
+iter = 50;% number of iterations
+
