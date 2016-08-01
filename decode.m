69521545997b4bcb9d3b863d182278c8ad304946
goldishs <simchagoldish@gmail.com>
2016-08-01 11:11:42 -0400

    add decode.m

---
+++ decode.m
@@ -0,0 +1,99 @@
+function [i,j,k,l] = decode(g, m, n, Q)
+
+% n is column
+% m is row
+
+
+
+% finding i first
+i = ceil(g/((4*Q-1) + (5*Q-1)*(n-2) + (3*Q-1)));
+rj =  g - ((4*Q-1) + (5*Q-1)*(n-2) + (3*Q-1))*(i-1);
+%
+% finding j 
+% if (rj <= ((4*Q-1) && i < m))
+%     j=1
+% elseif (4*Q <= rj <= ( (4*Q-1) + (n-2)*(5*Q-1)) && i < m)
+%     j = ceil((rj - (4*Q-1))/(5*Q-1))+1
+% elseif (i < m)
+%     j=n
+% elseif (i==m) 
+%     j = ceil(rj/(2*Q-1))
+% end
+
+
+if i<m;
+    if rj<4*Q;
+        j=1;
+    end
+    if 4*Q-1<rj;
+        if rj<4*Q-1+(n-2)*(5*Q-1)+1;
+            j=ceil((rj-(4*Q-1))/(5*Q-1))+1;
+        end
+        if rj>4*Q-1+(n-2)*(5*Q-1);
+            j=n;
+        end
+    end
+end
+if i==m;
+    j=ceil(rj/(2*Q-1));
+end
+
+
+% finding rk
+if (j==1 && i < m)
+    rk=rj;
+elseif (1<j && i<m)
+    rk = rj - (4*Q-1) - (j-2)*(5*Q-1);
+elseif (i==m)
+    rk = rj -(2*Q-1)*(j-1);
+else
+    x=5;
+end
+    
+% finding k 
+if (j==1 && i<m)
+    k= ceil(rk/(4));
+    
+elseif (1< j && j <n && i<m)
+    k = ceil(rk/(5));
+elseif (j==n && i<m)
+    k = ceil( rk/(3));
+elseif (i==m && j<n)
+    k = ceil(rk /(2));
+elseif(i==m && j==n)
+    k = rk;
+else
+    x=7;
+end
+    
+    
+    % finding l
+if (i < m) && (j == 1)
+
+    l = g-(((4*Q-1)+(n-2)*(5*Q-1)+(3*Q-1))*(i-1)+4*(k-1)); % i<m,j=1
+elseif (i < m) && (1 < j) && (j < n)
+
+    l = g-(((4*Q-1)+(n-2)*(5*Q-1)+(3*Q-1))*(i-1)+(4*Q-1)+(5*Q-1)*(j-2)+5*(k-1)); % i<m, 1<j<n
+elseif (i < m) && (j == n)
+
+    l = g-(((4*Q-1)+(n-2)*(5*Q-1)+(3*Q-1))*(i-1)+(4*Q-1)+(5*Q-1)*(n-2)+3*(k-1)); % i<m, j=n
+elseif (i == m) && (j < n)
+
+    l = g-(((4*Q-1)+(n-2)*(5*Q-1)+(3*Q-1))*(m-1)+(2*Q-1)*(j-1)+2*(k-1)); %i=m, j<n
+elseif (i==m && j==n)
+
+    %l = g-(((4*Q-1)+(n-2)*(5*Q-1)+(3*Q-1))*(m-1)+(2*Q-1)*(n-1))+k; %i=m, j=n
+    if (k==Q)
+        disp('ERROR')
+    else
+        l=1;
+    end
+end
+    
+    %
+    
+
+
+
+%return(i,j,k,l)
+end 
--- flood.m
+++ flood.m
@@ -16,65 +16,20 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %dimensions of our grid
-m = 2;%rows
-n = 4;%columns
-Q = 2;%Total number of layers
+m = 300;%rows
+n = 300;%columns
+Q = 50;%Total number of layers
 
 
 % number of iterations
 iter = 50;
 
-g=encode(2,1,2,2,m,n,Q)
-% finding i first
-i = ceil(g/((4*Q-1) + (5*Q-1)*(n-2) + (3*Q-1)))
-rj =  g - ((4*Q-1) + (5*Q-1)*(n-2) + (3*Q-1))*(i-1)
-% finding j 
-if (rj <= ((4*Q-1) && i < m))
-    j=1
-elseif (4*Q <= rj <= ( (4*Q-1) + (n-2)*(5*Q-1)) && i < m)
-    j = ceil((rj - (4*Q-1))/(5*Q-1))+1
-elseif (i < m)
-    j=n
-elseif (i==m) 
-    j = ceil(rj/(2*Q-1))
-end
-% finding rk
-if (j==1 && i < m)
-    rk=rj
-elseif (1<j && i<m)
-    rk = rj - (4*Q-1) - (j-2)*(5*Q-1)
-elseif (i==m)
-    rk = rj -(2*Q-1)*(j-1)
-else
-    x=5
-end
-    
-% finding k 
-if (j==1 && i<m)
-    k= ceil(rk/(4*Q-1))
-    
-elseif (1< j && j <n && i<m)
-    k = ceil(rk/(5*Q-1))
-elseif (j==n && i<m)
-    k = ceil( rk/(3*Q-1))
-elseif (i==m && j<n)
-    k = ceil(rk /(2*Q-1))
-elseif(i==m && j==n)
-    k = rk
-else
-    x=7
-end
-    
-    
-    % finding l
-    if (i < m) && (j == 1)
-    l = g-(((4*Q-1)+(n-2)*(5*Q-1)+(3*Q-1))*(i-1)+4*(k-1)); % i<m,j=1
-elseif (i < m) && (1 < j) && (j < n)
-    l = g-(((4*Q-1)+(n-2)*(5*Q-1)+(3*Q-1))*(i-1)+(4*Q-1)+(5*Q-1)*(j-2)+5*(k-1)); % i<m, 1<j<n
-elseif (i < m) && (j == n)
-    l = g-(((4*Q-1)+(n-2)*(5*Q-1)+(3*Q-1))*(i-1)+(4*Q-1)+(5*Q-1)*(n-2)+3*(k-1)); % i<m, j=n
-elseif (i == m) && (j < n)
-    l = g-(((4*Q-1)+(n-2)*(5*Q-1)+(3*Q-1))*(m-1)+(2*Q-1)*(j-1)+2*(k-1)); %i=m, j<n
-else
-    l = g-(((4*Q-1)+(n-2)*(5*Q-1)+(3*Q-1))*(m-1)+(2*Q-1)*(n-1))+k; %i=m, j=n
-end
+% test functions encode and undoing
+i = 100;
+j = 150;
+k = 25;
+l = 3;
+
+g=encode(i,j,k,l,m,n,Q);
+
+[i,j,k,l] = decode(g, m, n, Q);
