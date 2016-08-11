69f4251d6df3963f4f1897a990c8964f3b5e39a0
ely <elycbiggs@yahoo.com>
2016-08-11 16:54:27 -0400

    did stuff

--- flood.m
+++ flood.m
@@ -26,17 +26,17 @@ global Q
 
 % m, n, and Q should be 50,50,and 3 in our final version. 
 
-m = 50;%rows
-n = 50;%columns
+m = 20;%rows
+n = 20;%columns
 Q = 3;%Total number of layers
 V = ((4*Q-1)+(n-2)*(5*Q-1)+(3*Q-1))*(m-1)+(2*Q-1)*(n-1)+(Q-1);%Total number of variables
 
 
 % creating Elevations (these are randomly generated and are only to test if
 % the generating functions work. 
-T1 = 10*rand(m,n);
-T2 = 10*rand(m,n);
-T3 = 10*rand(m,n);
+T1 = 10 + 0.2*rand(m,n);
+T2 = 10 + 0.2*rand(m,n);
+T3 = 10 + 0.2*rand(m,n);
 
 E1 = T1+T2+T3;
 E2 = T2+T3;
@@ -46,11 +46,11 @@ E = vertcat(E1,E2,E3);
 T = vertcat(T1,T2,T3);
 
 C = rand(m*Q,n);
-S = 3*rand(m*Q,n);
+S = 1+ rand(m*Q,n);
 
 
 % number of iterations
-iter = 10;
+iter = 1;
 
 % test functions encode and undoing
 % i = 100;
