031cc285d19dbf81ce6ac0e8e5dd267bd4075d2b
makingmath <elycbiggs@yahoo.com>
2016-08-16 16:44:50 -0400

    changes

--- deltaS.m
+++ deltaS.m
@@ -1,16 +1,23 @@
-function [DS] = deltaS(x)
+
+function [DS] = deltaS(xnext)
 global Q
 global n
 global m
 
-DS = sparse(Q*m,n);
 
-for iter = 1:encode(m,n,Q-1,1)
-    matdsval = decode(iter);
+% What we need to do is create a function takes in every arrow pointing to
+% a node and calculates the total water that entered the node and total
+% water that left that node, for all nodes.
+
+
+DS = zeros(Q*m,n);
+
+for itera = 1:encode(m,n,Q-1,1)
+    matdsval = decode(itera);
     xx = matdsval(1,1);
     y = matdsval(1,2);
     z = matdsval(1,3);
-    t = (z-1) +xx;
-    DS(t,y) = DS(t,y) + x(iter);
+    t = ((z-1)*m) +xx;
+    DS(t,y) = DS(t,y) + xnext(itera);
 end
 end
